ef7e5143947127af019ba1cb2dea2564093dfdee
judeiras <deira361@gmail.com>
2025-01-09 17:35:35 +0300

    fixed.

--- client/menus/main.lua
+++ client/menus/main.lua
@@ -131,7 +131,7 @@ lib.callback.register('qbx_customs:client:vehicleProps', function()
     return lib.getVehicleProperties(vehicle)
 end)
 
-exports('openCustoms', function()
+exports('OpenMenu', function()
     openedWithExports = true
     if not cache.vehicle or inMenu then return false end
     vehicle = cache.vehicle
